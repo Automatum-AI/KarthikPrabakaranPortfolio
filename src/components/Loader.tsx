752150cbce28f895af164af4248db789812d66cc
Karthik Prabakaran <karthik@automatum.ai>
2025-10-16 16:49:53 +0530

    MVP3

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,9 +3,20 @@ import './Loader.css';
 
 const Loader: React.FC = () => (
   <div className="loader-container">
-    <div className="loader">
-      <div className="spinner"></div>
-      <span>Loading...</span>
+    <div className="space-loader">
+      {/* Animated stars */}
+      {[...Array(30)].map((_, i) => (
+        <div key={i} className="star" style={{
+          top: `${Math.random() * 100}%`,
+          left: `${Math.random() * 100}%`,
+          animationDelay: `${Math.random()}s`
+        }} />
+      ))}
+      {/* Planet */}
+      <div className="planet">
+        <div className="ring"></div>
+      </div>
+      <span className="space-text">Preparing for cosmic journey...</span>
     </div>
   </div>
 );
